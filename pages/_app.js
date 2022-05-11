b41fc08ceed81d1dda1a8b03fe4ffdae01f268e7
GabrielEsmeraldo <gabriel-esmeraldo@hotmail.com>
2022-05-11 15:17:25 -0300

    changing directory

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import '../styles/Globals.css'
+import './styles/Globals.css'
 
-import Layout from '../Components/Layout'
+import Layout from './Components/Layout'
 
 function MyApp({ Component, pageProps }) {
     return (
